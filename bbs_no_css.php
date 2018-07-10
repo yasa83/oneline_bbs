e26e53b9dd5f8a16e72d1661ecce4201c684248f
井上沙耶 <>
2018-07-10 12:03:30 +0800

    インジェクション前

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -1,5 +1,22 @@
 <?php
   // ここにDBに登録する処理を記述する
+    $nickname = htmlspecialchars($_POST['nickname']);
+    $comment = htmlspecialchars($_POST['comment']);
+
+
+    //１　データベースに接続する処理
+    $dsn = 'mysql:dbname=oneline_bbs;host=localhost';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->query('SET NAMES utf8');
+
+    //SQLに保存する
+    $sql = 'INSERT INTO `posts`(`nickname`, `comment`) VALUES ("'. $nickname.'","'.$comment.'")';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+
+    $dbh = null;
 
 ?>
 <!DOCTYPE html>
